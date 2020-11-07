db90678fcce5443df9b81f860a83cea70532d638
azharfatrr <m.azhar.faturahman@gmail.com>
2020-11-07 14:04:03 +0700

    Create Documents
    Database document

---
+++ get_doc.py
@@ -0,0 +1,56 @@
+import requests
+import os.path
+from bs4 import BeautifulSoup
+
+link_doc = "https://kolom.kompas.com/"
+
+# Make a request to the website
+r = requests.get(link_doc)
+
+# Create an object to parse the HTML format
+soup = BeautifulSoup(r.content, 'html.parser')
+
+# Retrieve all popular news links (Fig. 1)
+link = []
+for i in soup.find('div', {'class':'most__wrap'}).find_all('a'): #Berkaitan dengan struktur data web
+    link.append(i['href']+'?page=all')                           #Biar pagenya ditampilkan semua
+    
+# For each link, we retrieve paragraphs from it, combine each paragraph as one string, 
+# and save it to documents (Fig. 2)
+
+document = []
+for i in link:
+    #Dapatkan info dalam link
+    r = requests.get(i)
+    
+    #Parse HTML
+    soup = BeautifulSoup(r.content, 'html.parser')
+    
+    #Dapatkan tiap paragraph
+    sen = []
+    for i in soup.find('div', {'class':'read__content'}).find_all('p'):
+        sen.append(i.text)
+    document.append(' '.join(sen))
+
+
+# Simpan dokumen
+j = 1
+for doc in document:
+    path_cek = "Documents/"
+    
+    namefile = "doc" + str(j) +".txt"
+    path_cek += namefile
+    
+    while (os.path.exists(path_cek)): #Cek nama document sudah ada atau belum
+        j += 1
+        path_cek = "Documents/"
+        namefile = "doc" + str(j) +".txt"
+        path_cek += namefile
+    
+    path = path_cek
+    file = open(path,'w')
+    file.write(doc)
+    file.close()
+    j += 1
+    
+print("Berhasil mengambil data dari " + link_doc)
