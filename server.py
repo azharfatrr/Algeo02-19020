86e2de863ef332f3d00c0bc2da8f5aa7134136a6
raf555 <irsoekarno888@gmail.com>
2020-11-10 18:42:13 +0700

    fix dikit

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect
 from backend import main
 
 # init
@@ -18,6 +18,8 @@ def perihal():
 @app.route('/search', methods=['GET'])
 def search():
   """ api search """
+  if not request.args.get('querydoc') or not request.args.get('querytype') or not request.args.get('querysearch'):
+    return redirect("/")
   a = main.main(request.args.get('querysearch'), int(request.args.get('querydoc')), int(request.args.get('querytype')))
-  return render_template('table.html', data=a, len=[len(a[0]), len(a[1])])
+  return render_template('table.html', data=a, len=[len(a[0]), len(a[1]), len(a[1][0])])
   #return jsonify(a)
