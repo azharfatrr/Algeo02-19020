481cb327cfb996c9ef43cefd69cf97640f14ba9c
raf555 <irsoekarno888@gmail.com>
2020-11-10 17:25:38 +0700

    frontend gan

--- server.py
+++ server.py
@@ -9,9 +9,15 @@ app = Flask(__name__, static_folder='frontend/static', template_folder='frontend
 def homepage():
   """ index """
   return render_template('index.html')
+
+@app.route('/perihal')
+def perihal():
+  """ perihal """
+  return render_template('perihal.html')
     
-@app.route('/search', methods=['GET', 'POST'])
+@app.route('/search', methods=['GET'])
 def search():
   """ api search """
   a = main.main(request.args.get('querysearch'), int(request.args.get('querydoc')), int(request.args.get('querytype')))
   return render_template('table.html', data=a, len=[len(a[0]), len(a[1])])
+  #return jsonify(a)
