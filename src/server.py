dfb68ac86dd69f0b336735b683d8622aca34ebf5
raf555 <irsoekarno888@gmail.com>
2020-11-13 10:05:48 +0700

    nambahin komen

--- src/server.py
+++ src/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request, redirect
+from flask import Flask, render_template, request, redirect
 from backend import main
 
 # init
@@ -7,29 +7,40 @@ app = Flask(__name__, static_folder='frontend/static', template_folder='frontend
 # router
 @app.route('/')
 def homepage():
-  """ index """
+  """ halaman depan """
   return render_template('index.html')
 
 @app.route('/perihal')
 def perihal():
-  """ perihal """
+  """ halaman untuk perihal """
   return render_template('perihal.html')
     
 @app.route('/search', methods=['GET'])
 def search():
-  """ mencari dokumen berdasarkan query """
+  """
+  halaman untuk mendapatkan hasil pencarian berdasarkan tiga paramater
+  querysearch = kata kunci pencarian
+  querytype = tipe pencarian (cepat atau akurat (dengan stemming / lambat))
+  querydoc = jumlah dokumen yang akan dicari
+  """
+  doc = 160 # jumlah dokumen
+
+  # validasi request
   if not request.args.get('querydoc') or not request.args.get('querytype') or not request.args.get('querysearch'):
     return redirect("/")
-  if not(int(request.args.get('querydoc')) > 0 and int(request.args.get('querydoc')) <= 150):
+  if not(int(request.args.get('querydoc')) > 0 and int(request.args.get('querydoc')) <= doc):
     return redirect("/")
   if int(request.args.get('querytype')) != 1 and int(request.args.get('querytype')) != 0:
     return redirect("/")
+
+  # melewati validasi
+  # mengembalikan data hasil pencarian dan parameter ke table.html untuk di-render
   a = main.main(request.args.get('querysearch'), int(request.args.get('querydoc')), int(request.args.get('querytype')))
   b = request.args.get('querysearch')
   c = request.args.get('querydoc')
   d = request.args.get('querytype')
+  
   return render_template('table.html', data=a, len=[len(a[0]), len(a[1]), len(a[1][0])], query=b, doc=int(c), type=int(d))
-  #return jsonify(a)
 
 @app.route('/doc/<path:path>')
 def send_txt(path):
